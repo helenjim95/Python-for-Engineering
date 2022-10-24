b2a25763c5acbf56c8f810c37a9b36c0b0a27909
ge32goy <hanyin.chen@tum.de>
2022-10-24 22:08:44 +0200

    exercise2

--- chapter_2/ex2_loop/main.py
+++ chapter_2/ex2_loop/main.py
@@ -6,3 +6,4 @@ for i in range(0, 316, 5):
         print("sin({:.2f}) = {:.2f}, near null".format(number, result))
     else:
         print("sin({:.2f}) = {:.2f}".format(number, result))
+    print()
