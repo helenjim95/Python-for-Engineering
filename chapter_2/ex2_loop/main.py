ae68438c74009316fa30201030b7a731d298c3d1
ge32goy <hanyin.chen@tum.de>
2022-10-24 22:16:37 +0200

    exercise2

--- chapter_2/ex2_loop/main.py
+++ chapter_2/ex2_loop/main.py
@@ -2,7 +2,7 @@ import math
 for i in range(0, 316, 5):
     number = i/100
     result = math.sin(number)
-    if result < 0.1:
+    if -0.1 <= result <= 0.1:
         print("sin({:.2f}) = {:.2f}, near null".format(number, result))
     else:
         print("sin({:.2f}) = {:.2f}".format(number, result))
