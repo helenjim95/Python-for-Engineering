404264aef24b68bf9a09959705de1d3c76e46c3f
ge32goy <hanyin.chen@tum.de>
2022-10-24 23:50:34 +0200

    exercise3

---
+++ chapter_2/ex4_strain_gauge/main.py
@@ -0,0 +1,31 @@
+import math
+
+with open("strain_gauge_processed.csv", "w") as output:
+    with open("strain_gauge_rosette.csv", "r") as file:
+        r2_1_list = []
+        r2_2_list = []
+        r2_3_list = []
+        e_1_list = []
+        e_2_list = []
+        first_line = file.readline().replace("\n", "").split(sep="	")
+        first_line.append("e_1")
+        first_line.append("e_2")
+        output.write(";".join(first_line))
+        output.write("\n")
+        for line in file:
+            data = line.split(sep="	")
+            # need to change decimal to 9
+            r2_1 = float("{:.9f}".format(float(data[0])))
+            r2_2 = float("{:.9f}".format(float(data[1])))
+            r2_3 = float("{:.9f}".format(float(data[2])))
+            r2_1_list.append(r2_1)
+            r2_2_list.append(r2_2)
+            r2_3_list.append(r2_3)
+            e_1 = 0.5 * (r2_1 + r2_3 + math.sqrt(2 * ((r2_1 - r2_2) ** 2 + (r2_2 - r2_3) ** 2)))
+            e_2 = 0.5 * (r2_1 + r2_3 - math.sqrt(2 * ((r2_1 - r2_2) ** 2 + (r2_2 - r2_3) ** 2)))
+            e_1_list.append(e_1)
+            e_2_list.append(e_2)
+
+            data_list = [str(float("{:.9f}".format(float(r2_1)))), str(float("{:.9f}".format(float(r2_2)))), str(float("{:.9f}".format(float(r2_3)))), str(float("{:.9f}".format(float(e_1)))), str(float("{:.9f}".format(float(e_2))))]
+            output.write(";".join(data_list))
+            output.write("\n")
