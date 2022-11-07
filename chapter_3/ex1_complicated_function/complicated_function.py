e98905c8f16366edf2a25458858ceb9d026797e4
ge32goy <hanyin.chen@tum.de>
2022-11-07 18:40:35 +0100

    output

--- chapter_3/ex1_complicated_function/complicated_function.py
+++ chapter_3/ex1_complicated_function/complicated_function.py
@@ -7,15 +7,18 @@ def convert_to_dtype(*args, dtype, debug=False):
         try:
             result.append(dtype(arg))
         except ValueError as e:
-            exc_info = sys.exc_info()
             if debug:
                 print(f"ErrorMessage: Can't convert '{arg}' at index {index} # this is from the debug-flag")
+                exc_info = sys.exc_info()
+                print(traceback.format_exc())
+                raise ValueError
             else:
-                pass
-            print(traceback.format_exc())
-            break
+                exc_info = sys.exc_info()
+                print(traceback.format_exc())
     if len(result) == len([*args]):
         return result
+    else:
+        pass
 
 
 if __name__ == "__main__":
@@ -25,12 +28,21 @@ if __name__ == "__main__":
     print(convert_to_dtype((1,0), "a", 15.1516, dtype=str))
     # [’(1, 0)’, ’a’, ’15.1516’]
 
+    # need to not return anyting if error occurs
+    try:
+        print(convert_to_dtype(5, "a", dtype=int, debug=False))
+    except ValueError as e:
+        pass
 
-    print(convert_to_dtype(5, "a", dtype=int, debug=False))
+
     # Traceback
     # ValueError: invalid literal for int() with base 10: ’a
+    try:
+        print(convert_to_dtype(5, "a", dtype=int, debug=True))
+    except ValueError as e:
+        pass
 
-    print(convert_to_dtype(5, "a", dtype=int, debug=True))
+
     # ErrorMessage: Can’t convert ’a’ at index 1 # this is from the debug-flag
     # Traceback
     # ValueError: invalid literal for int() with base 10: ’a'
