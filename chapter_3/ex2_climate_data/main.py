e05a835da244f14be35b3972b206f9f043680f0d
ge32goy <hanyin.chen@tum.de>
2022-11-07 17:43:24 +0100

    output

--- chapter_3/ex2_climate_data/main.py
+++ chapter_3/ex2_climate_data/main.py
@@ -1,6 +1,9 @@
 import os
 import json
+
+data_by_country = {}
 def read_output_file():
+    global data_by_country
     # read in all the files in a dictionary structure
     # key is the abbreviated country-code, the value is a list of all the data-lines included in the file converted to float
     file_list = os.listdir("__files")
@@ -18,5 +21,17 @@ def read_output_file():
         # Output the dictionary to a json-ﬁle raw_data.json with the json.dump-function (help: import json; help(json.dump).
         json.dump(data_by_country, output)
 
+def aggregate():
+    with open('aggregated_data.json', 'w+') as output:
+        output_by_country = {}
+        for key, values in data_by_country.items():
+            t_avg = sum(values) / len(values)
+            t_max = max(values)
+            t_min = min(values)
+            output_by_country[key] = {'t_avg': t_avg, 't_max' : t_max, 't_min' : t_min}
+        json.dump(data_by_country, output)
+
+
 if __name__ == "__main__":
     read_output_file()
+    aggregate()
