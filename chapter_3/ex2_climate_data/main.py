47a6f9c49df11d9452cfac942b5030cb0b5f9e3f
ge32goy <hanyin.chen@tum.de>
2022-11-13 10:31:14 +0100

    output

--- chapter_3/ex2_climate_data/main.py
+++ chapter_3/ex2_climate_data/main.py
@@ -9,18 +9,17 @@ def read_output_file():
     # key is the abbreviated country-code, the value is a list of all the data-lines included in the file converted to float
     file_list = os.listdir("__files")
     with open('raw_data.json', 'w') as output:
-        data_by_country_unsorted = {}
+        data_by_country = {}
         for filename in file_list:
             with open(f"__files/{filename}", "r") as file:
                 values = []
                 country_code = filename.replace(".csv", "")
                 for value in file:
                     values.append(float(value))
-                data_by_country_unsorted[country_code] = values
+                data_by_country[country_code] = values
         # Order the dictionary by the keys
-        data_by_country = data_by_country_unsorted
         # Output the dictionary to a json-ﬁle raw_data.json with the json.dump-function (help: import json; help(json.dump).
-        json.dump(data_by_country, output)
+        json.dump(data_by_country, output, sort_keys=True)
 
 def aggregate():
     global output_by_country
@@ -30,7 +29,7 @@ def aggregate():
             t_max = max(values)
             t_min = min(values)
             output_by_country[key] = {'t_avg': t_avg, 't_max': t_max, 't_min': t_min}
-        json.dump(output_by_country, output)
+        json.dump(output_by_country, output, sort_keys=True)
 
 
 if __name__ == "__main__":
